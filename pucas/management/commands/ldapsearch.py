d8655e9c12e992dcc8c3b42a2dfd4d246adcade6
rlskoeser <rebecca.s.koeser@emory.edu>
2016-12-20 16:59:37 -0500

    Simple ldap search manage command, to test configuration
    [https://app.asana.com/0/0/233732076199652]

---
+++ pucas/management/commands/ldapsearch.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from pucas.ldapsearch import LDAPSearch, LDAPSearchException
+
+
+class Command(BaseCommand):
+    help = 'Look up one or more users in LDAP by netid'
+
+    def add_arguments(self, parser):
+        parser.add_argument('netid', nargs='+')
+
+    def handle(self, *args, **options):
+        ldap_search = LDAPSearch()
+        for netid in options['netid']:
+            print('\nLooking for %s...' % netid)
+            try:
+                info = ldap_search.find_user(netid)
+                print(info)
+                # display attributes configured in settings
+                for attr in settings.PUCAS_LDAP['ATTRIBUTES']:
+                    print('%-15s %s' % (attr, getattr(info, attr)))
+            except LDAPSearchException as err:
+                print(err)
+
