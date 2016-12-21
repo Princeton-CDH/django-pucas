52eb6a3d8188c5e8fbabbe4f4822d0c4ececf48b
rlskoeser <rebecca.s.koeser@emory.edu>
2016-12-21 15:52:53 -0500

    Add option to see all available ldap attributes via ldapsearch command

--- pucas/management/commands/ldapsearch.py
+++ pucas/management/commands/ldapsearch.py
@@ -9,16 +9,24 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('netid', nargs='+')
+        parser.add_argument('--all', '-a', action='store_true',
+            help='Retrieve all available LDAP attributes')
 
     def handle(self, *args, **options):
         ldap_search = LDAPSearch()
+        print(options['all'])
         for netid in options['netid']:
             print('\nLooking for %s...' % netid)
             try:
-                info = ldap_search.find_user(netid)
-                # display attributes configured in settings
-                for attr in settings.PUCAS_LDAP['ATTRIBUTES']:
-                    print('%-15s %s' % (attr, getattr(info, attr)))
+                info = ldap_search.find_user(netid, all_attributes=options['all'])
+                # if all attributes were requested, just print the returned
+                # ldap search object
+                if options['all']:
+                    print(info)
+                # otherwise, display attributes configured in settings
+                else:
+                    for attr in settings.PUCAS_LDAP['ATTRIBUTES']:
+                        print('%-15s %s' % (attr, getattr(info, attr)))
             except LDAPSearchException as err:
                 print(err)
 
