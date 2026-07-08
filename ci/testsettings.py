7988f11b63cbc63153ba213f31347510de818fc3
hao <97079365+tanhaow@users.noreply.github.com>
2026-07-08 15:19:55 -0400

    Add admin apps to CI test settings

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -9,8 +9,13 @@ DATABASES = {
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.admin',
+    'django.contrib.messages',
+    'django.contrib.sessions',
     'django_cas_ng',
     'pucas',
 )
+
+ROOT_URLCONF = 'pucas.cas_urls'
 
 # SECRET_KEY = ''
